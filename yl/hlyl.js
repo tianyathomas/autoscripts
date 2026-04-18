ace592150672cef9f6a61405e7d9696e231ea715
tianyathomas <tianya3113@gmail.com>
2026-04-18 12:00:45 +0800

    add: 活力伊利小程序签到脚本

---
+++ yl/hlyl.js
@@ -0,0 +1,113 @@
+/*
+------------------------------------------
+@Author: sm
+@Date: 2024.06.07 19:15
+@Description: 活力伊利小程序签到
+cron: 30 8 * * *
+------------------------------------------
+#Notice: 
+变量名 hlyl
+抓取 https://msmarket.msx.digitalyili.com/请求头access-token 多账户&或换行
+
+变量名 hlyl
+抓取 https://msmarket.msx.digitalyili.com/请求头access-token 多账户&或换行
+*/
+
+const { Env } = require("../tools/env")
+const $ = new Env("活力伊利小程序");
+let ckName = `hlyl`;
+const strSplitor = "#";
+const axios = require("axios");
+const defaultUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.31(0x18001e31) NetType/WIFI Language/zh_CN miniProgram"
+
+
+class Task {
+ constructor(env) {
+ this.index = $.userIdx++
+ this.user = env.split(strSplitor);
+ this.token = this.user[0];
+
+ }
+
+ async run() {
+
+ await this.signIn()
+ }
+
+ async signIn() {
+ let options = {
+ method: 'POST',
+ url: `https://msmarket.msx.digitalyili.com/gateway/api/member/daily/sign`,
+ headers: {
+
+ "accept": "*/*",
+ "accept-language": "zh-CN,zh;q=0.9",
+ "access-token": "" + this.token,
+ "atv-page": "",
+ "content-type": "application/json",
+ "forward-appid": "",
+ "priority": "u=1, i",
+ "register-source": "",
+ "scene": "1145",
+ "sec-fetch-dest": "empty",
+ "sec-fetch-mode": "cors",
+ "sec-fetch-site": "cross-site",
+ "source-type": "",
+ "tenant-id": "",
+ "xweb_xhr": "1"
+
+
+ , data: {
+
+ }
+ }
+ }
+ let { data: result } = await axios.request(options);
+ if (result?.status == true) {
+ $.log(`🌸账号[${this.index}]` + `🕊签到获得${result.data.dailySign.bonusPoint}分🎉`);
+ } else {
+ $.log(`🌸账号[${this.index}] 签到-失败:${result.error}❌`)
+ }
+
+
+
+
+ }
+
+
+
+
+
+
+
+
+}
+
+!(async () => {
+ await getNotice()
+ $.checkEnv(ckName);
+
+ for (let user of $.userList) {
+ await new Task(user).run();
+ }
+})()
+ .catch((e) => console.log(e))
+ .finally(() => $.done());
+
+async function getNotice() {
+ try {
+ let options = {
+ url: `https://ghproxy.net/https://raw.githubusercontent.com/smallfawn/Note/refs/heads/main/Notice.json`,
+ headers: {
+ "User-Agent": defaultUserAgent,
+ },
+ timeout: 3000
+ }
+ let {
+ data: res
+ } = await axios.request(options);
+ $.log(res)
+ return res
+ } catch (e) { }
+
+}
